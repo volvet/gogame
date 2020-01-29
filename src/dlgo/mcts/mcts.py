5583a8ad544f69125b13055bccbd501c29c3e93f
volvet <volvet@163.com>
2020-01-29 16:21:11 +0800

    mcts

---
+++ src/dlgo/mcts/mcts.py
@@ -0,0 +1,21 @@
+
+import math
+import random
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from dlgo import agent
+from dlgo.gotypes import Player
+from dlgo.utils import coords_from_point
+
+__all__ = [
+    'MCTSAgent',
+]
+
+def mcts_test():
+    pass
+
+if __name__ == '__main__':
+    mcts_test()
+
--- src/dlgo/zobrist.py
+++ src/dlgo/zobrist.py
@@ -1,7 +1,9 @@
 
 
 import random
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dlgo.gotypes import Player, Point
 
 
