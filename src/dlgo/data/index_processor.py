4716f55a5f822e571d089722f4aa4702bc23d990
volvet <volvet2002@gmail.com>
2020-05-24 22:34:25 +0800

    download sgf files

---
+++ src/dlgo/data/index_processor.py
@@ -0,0 +1,61 @@
+
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import sys
+import multiprocessing
+import six
+
+if sys.version_info[0] == 3:
+  from urllib.request import urlopen, urlretrieve
+else:
+  from urllib import urlopen, urlretrieve
+
+
+class KGSIndex:
+  def __init__(self, kgs_url='https://u-go.net/gamerecords/',
+               index_page='kgs_index.html',
+               data_directory='data'):
+    self.kgs_url = kgs_url
+    self.index_page= index_page
+    self.file_info= []
+    self.urls = []
+    self.load_index()
+    
+  def create_index_page(self):
+    if os.path.isfile(self.index_page):
+      print('>>> Reading cached index page')
+      index_file = open(self.index_page, 'r')
+      index_contents = index_file.read()
+      index_file.close()
+    else:
+      print('>>> Downloading index page')
+      fp = urlopen(self.kgs_url)
+      data = six.text_type(fp.read())
+      fp.close()
+      index_contents = data
+      index_file = open(self.index_page, 'w')
+      index_file.write(index_contents)
+      index_file.close()
+    return index_contents
+  
+  def load_index(self):
+    index_contents = self.create_index_page()
+    split_page = [item for item in index_contents.split('<a href="') if item.startswith('https://')]
+    for item in split_page:
+      download_url = item.split('">Download')[0]
+      if download_url.endswith('.tar.gz'):
+        #print(download_url)
+        self.urls.append(download_url)
+    for url in self.urls:
+      filename = os.path.basename(url)
+      split_file_name = filename.split('-')      
+      num_games = int(split_file_name[len(split_file_name) - 2])
+      print(filename + ' ' + str(num_games))
+      self.file_info.append({'url': url, 'filename':filename, 'num_games': num_games})
+    pass
+
+if __name__ == '__main__':
+  print('index_processor')
+  index = KGSIndex()
+  #print(index)
